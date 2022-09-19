1f8c4f4b91406e383b564999302c5fc975ddc745
Aadigha-git <ba.aaditya03@gmail.com>
2022-09-19 18:16:34 +0530

    solid rectangle(patterns)

--- DataStructures/src/patterns/SolidRectangle.java
+++ DataStructures/src/patterns/SolidRectangle.java
@@ -5,6 +5,21 @@ public class SolidRectangle {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		printRectangle(5, 5);
 	}
 
+	public static void printRectangle(int width, int height) {
+		int i;
+		int j;
+		for (i = 0; i < height; i++) {
+			for (j = 0; j < width; j++) {
+				printStars(width);
+			}
+			System.out.println("");
+		}
+	}
+
+	public static void printStars(int amount) {
+		System.out.print("* ");
+	}
 }
