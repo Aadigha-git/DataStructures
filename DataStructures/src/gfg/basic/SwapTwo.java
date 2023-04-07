67c1c77fd15a71f99394e18d5b459f0f877046b8
Aadigha-git <ba.aaditya03@gmail.com>
2023-04-07 11:42:25 +0530

    Swapping two numbers without using a third variable in a single line

---
+++ DataStructures/src/gfg/basic/SwapTwo.java
@@ -0,0 +1,31 @@
+package gfg.basic;
+
+import java.util.Scanner;
+
+public class SwapTwo {
+
+	public static void main(String[] args) {
+		
+		/*Simplest way to swap two numbers without using a third variable in a single line.
+		  					a = (a + b) - (b = a) 
+		 This gives us "a + b - a", which simplifies to just "b", which we are equating to a */
+		
+		int a, b;
+
+		System.out.println("Enter integer 'a': ");
+		Scanner scn = new Scanner(System.in);
+
+		a = scn.nextInt();
+
+		System.out.println("Enter integer 'b': ");
+		b = scn.nextInt();
+
+		System.out.println("Before swapping Value of a is " + a + " and Value of b is " + b);
+
+		a = (a + b) - (b = a);
+
+		System.out.println("After Swapping Value of a is " + a + " and Value of b is " + b);
+		scn.close();
+
+	}
+}
